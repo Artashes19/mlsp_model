e70875f234479276be8823dbf03929fc9d1726cc
Rafayel Mkrtchyan <rafayelmkrtchyan12@gmail.com>
2026-01-13 12:55:12 +0400

    Bugfix in UNet

--- src/networks/mlsp_unet.py
+++ src/networks/mlsp_unet.py
@@ -73,7 +73,7 @@ class UNetModel(nn.Module):
     def forward(self, x):
         features = self.unet.encoder(x)
         features[-1] = self.aspp(features[-1])
-        decoder_output = self.unet.decoder(*features)
+        decoder_output = self.unet.decoder(features)
         logits = self.unet.segmentation_head(decoder_output)
         
         output = logits.squeeze(1)
