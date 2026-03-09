7b4bd5d176d4d494a13999458e4f6c0775980ae3
Artashes19 <artashes.mkrtchyan.2004@gmail.com>
2026-03-10 01:40:14 +0400

    test(dsa): cover fp8 scaffold import

--- tests/test_dsa_2d_indexer.py
+++ tests/test_dsa_2d_indexer.py
@@ -3,3 +3,9 @@ def test_dsa_test_scaffold_exists():
     from tests.helpers import dsa_reference
 
     assert hasattr(dsa_reference, "__file__")
+
+
+def test_fp8_test_scaffold_exists():
+    from tests.helpers import fp8_reference
+
+    assert hasattr(fp8_reference, "__file__")
